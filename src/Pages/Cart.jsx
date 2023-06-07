2a577755865861063995ce8082710c6f4a03bb23
Abd0allam <abdo12345allam@gmail.com>
2023-06-07 16:48:37 +0300

    migrated

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,64 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
 import "./Cart.css";
 
 export default function Cart() {
   const { cartItems } = useSelector((state)=>state.CartSlice);
 
   // group items by name and calculate total price and quantity for each group
-  const groupedItems = cartItems.reduce((groups, item) => {
-    const group = groups.find((g) => g.name === item.name);
-    if (group) {
-      group.quantity += 1;
-      group.price += item.price;
-    } else {
-      groups.push({
-        name: item.name,
-        price: item.price,
-        quantity: 1,
-        image: item.image,
-      });
-    }
-    return groups;
-  }, []);
+  const [groupedItems, setGroupedItems] = useState(
+    cartItems.reduce((groups, item) => {
+      const group = groups.find((g) => g.name === item.name);
+      if (group) {
+        group.quantity += 1;
+        group.price += item.price;
+      } else {
+        groups.push({
+          name: item.name,
+          price: item.price,
+          quantity: 1,
+          image: item.image, // add image to the group object
+        });
+      }
+      return groups;
+    }, [])
+  );
 
-  // console.log(groupedItems);
+  // increment quantity and price of a group
+  const incrementGroup = (index) => {
+    const newGroups = [...groupedItems];
+    newGroups[index].quantity += 1;
+    newGroups[index].price += newGroups[index].price / (newGroups[index].quantity - 1);
+    setGroupedItems(newGroups);
+  };
+
+  // decrement quantity and price of a group or remove group if quantity reaches 0
+  const decrementGroup = (index) => {
+    const newGroups = [...groupedItems];
+    if (newGroups[index].quantity === 1) {
+      // enable - button when quantity is 1
+      newGroups[index].quantity -= 1;
+      newGroups[index].price -= newGroups[index].price;
+      setGroupedItems(newGroups.filter((item) => item.quantity !== 0)); // remove group from array if quantity reaches 0
+    } else {
+      // decrement quantity and price if quantity is greater than 1
+      newGroups[index].quantity -= 1;
+      newGroups[index].price -= newGroups[index].price / (newGroups[index].quantity + 1);
+      setGroupedItems(newGroups);
+    }
+  };
+
+
 
   return (
-    
-      <div style={{height:"100vh"}}>
-        { groupedItems.length > 0 ? ( <>
-        <h2 style={{ textAlign: "center", color: "red" }}>Cart</h2>
-        <div className="card-group">
-          {groupedItems.map((item, index) => (
-            <Card key={item.name} className="card-item">
-              <div className="card-image">
-                <Card.Img variant="top" src={item.image} />
-              </div>
-              <div className="card-details">
-                <Card.Title>{item.name}</Card.Title>
-                <Card.Text>
-                  Price: {item.price} <br />
-                  Quantity: {item.quantity}
-                </Card.Text>
-                <div className="card-buttons">
-                  <Button variant="primary" disabled>
-                    +
-                  </Button>{" "}
-                  <Button variant="danger" disabled>
-                    -
-                  </Button>
+    <div className="container">
+      <h2 style={{ textAlign: "center", color: "red" }}>Cart</h2>
+      <div className="row">
+        {groupedItems.map((item, index) => (
+          <div key={item.name} className="col-12 col-md-6 col-lg-4">
+            <div className="card">
+              <img
+                className="card-img-top"
+                src={item.image}
+                style={{ height: '200px', width: '100%', objectFit: 'contain' }}
+                alt=""
+              />
+              <div className="card-body">
+                <h5 className="card-title" style={{ height: '48px', overflow: 'hidden' }}>
+                  {item.name}
+                </h5>
+                <div className="d-flex my-2 justify-content-between">
+                  <label className="price-label">Price : {item.price.toFixed(2)}</label>
+                  <label className="quantity-label">Quantity : {item.quantity}</label>
+                </div>
+                <div className="d-flex justify-content-between">
+                  
+                  <button className="btn btn-primary" onClick={() => incrementGroup(index)}>+</button>
+                  <button className="btn btn-danger" onClick={() => decrementGroup(index)} disabled={item.quantity <= 0}>-</button>
                 </div>
               </div>
-            </Card>
-          ))}
-        </div>
-        </>
-)
-         : (<h2 style={{ textAlign: "center", color: "red" }}>Your cart is empty</h2>) }
+            </div>
+          </div>
+        ))}
       </div>
-    );
-  
+    </div>
+  );
 }
